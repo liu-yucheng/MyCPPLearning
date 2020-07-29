a55507e4de515e40e6fe3f1a741eec4357ee9a69
liu-yucheng <liu.r.yucheng@gmail.com>
2020-07-29 20:15:15 +0800

    Added startup files

---
+++ README.md
@@ -0,0 +1,4 @@
+# MyCPPLearning
+
+Code I have written or reviewed while I am learning C++.
+
---
+++ p1-structure-of-a-program/main.cpp
@@ -0,0 +1,12 @@
+/* Ref:
+ * http://www.cplusplus.com/doc/tutorial/program_structure/ 
+ * A conventional Hello-World C++ program. */
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+    cout << "Hello world!" << endl;
+    return 0;
+}
