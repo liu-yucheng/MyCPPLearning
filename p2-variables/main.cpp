9c4b06dd8e4fa6283d029f7982cac9c20e869925
liu-yucheng <liu.r.yucheng@gmail.com>
2020-08-01 15:42:18 +0800

    Updated 1 part

--- p2-variables/main.cpp
+++ p2-variables/main.cpp
@@ -15,9 +15,31 @@ int main(int argc, char **argv) {
     b = 2;
     // Increment variable a
     a += 1;
-    // Calculate result
+    // Calculate and print result
     result = a - b;
     cout << result << endl;
+    // Declare some other variables
+    int a2 = 5, b2 (3), c2 {2};
+    // Mutate a2
+    a2 = a2 + b2;
+    // Calculate and print result
+    result = a2 - c2;
+    cout << result << endl;
+    // Declare an auto variable
+    auto a3 = a2;
+    // Print the auto variable
+    cout << a3 << endl;
+    // Declare a decltype variable
+    decltype(a3) a4 = a3;
+    // Print the decltype variable
+    cout << a4 << endl;
+    // Declare a string variable
+    string s = "Initial content";
+    // Print the string variable
+    cout << s << endl;
+    // Mutate the string
+    s = "Mutated content";
+    cout << s << endl;
     
     return 0;
 }
