cde07f1c726fc95950f26750af2c41a861f6ebc9
liu-yucheng <liu.r.yucheng@gmail.com>
2020-08-05 17:10:48 +0800

    Updated 1 part

--- p5-basic-io/main.cpp
+++ p5-basic-io/main.cpp
@@ -8,18 +8,22 @@
 using namespace std;
 
 int main(int argc, char **argv) {
+    // Take an integer from keyboard input
     int curIn;
     cout << "Enter an integer: ";
     cin >> curIn;
+    // Ignore the buffered new line character
     cin.ignore();
     cout << "The integer is: " << curIn << '\n' << endl;
 
+    // Take a word from keyboard input
     string curIn2;
     cout << "Enter a word: ";
     cin >> curIn2;
     cin.ignore();
     cout << "The word is: " << curIn2 << '\n' << endl;
 
+    // Take 2 integers from keyboard input
     int curIn3, curIn4;
     cout << "Enter 2 integers:\n";
     cin >> curIn3 >> curIn4;
@@ -27,12 +31,14 @@ int main(int argc, char **argv) {
     cout << "The 1st integer is: " << curIn3 << '\n';
     cout << "The 2nd integer is: " << curIn4 << '\n' << endl;
 
+    // Take a line from keyboard input
     string curIn5;
     cout << "Enter a line:\n";
     getline(cin, curIn5);
     cout << "The line is:\n";
     cout << curIn5 << '\n' << endl;
 
+    // Parse an integer from a string
     string numStr = "1234";
     int num;
     cout << "Parsing integer from string...\n";
